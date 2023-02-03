5ea8540647302dbba6faf18a14016b7becc14444
Lucas Correia <lucas.correia@ftrack.com>
2023-02-03 16:22:50 +0100

    Don't duplicate interfaces

--- src/util/session.ts
+++ src/util/session.ts
@@ -1,12 +1,7 @@
 import { Session } from "@ftrack/api";
 import { getPreferenceValues } from "@raycast/api";
+import { Preferences } from "../types";
 
-interface Preferences {
-  ftrackServerUrl: string;
-  ftrackApiUser: string;
-  ftrackApiKey: string;
-  assignedTasksFilter: string;
-}
 const preferences = getPreferenceValues<Preferences>();
 
 export const session = new Session(
